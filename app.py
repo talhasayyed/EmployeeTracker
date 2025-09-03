0692d935718fdf6a3d4b0f85bbe0c38adb9af322
tsayyed <tsayyed@nttglobal.net>
2025-09-03 18:23:44 +0530

    data changed

--- app.py
+++ app.py
@@ -22,10 +22,11 @@ def pie_only():
 def get_pie_data():
     # Dummy data for pie chart
     data = [
-        {"name": "Apples", "value": 25},
-        {"name": "Bananas", "value": 15},
-        {"name": "Cherries", "value": 30},
-        {"name": "Dates", "value": 10},
+        {"name": "talha", "value": 20},
+        {"name": "amit", "value": 15},
+        {"name": "sj", "value": 20},
+        {"name": "deepak", "value": 15},
+        {"name": "vivek", "value": 10},
         {"name": "Elderberries", "value": 20}
     ]
     return jsonify(data)
@@ -34,11 +35,11 @@ def get_pie_data():
 @app.route("/line_pie_data")
 def get_data():
     dataset = [
-        ["product", "2012", "2013", "2014", "2015", "2016", "2017"],
-        ["Milk Tea", 56.5, 82.1, 88.7, 70.1, 53.4, 85.1],
-        ["Matcha Latte", 51.1, 51.4, 55.1, 53.3, 73.8, 68.7],
-        ["Cheese Cocoa", 40.1, 62.2, 69.5, 36.4, 45.2, 32.5],
-        ["Walnut Brownie", 25.2, 37.1, 41.2, 18, 33.9, 49.1]
+        ["number of tickets", "2012", "2013", "2014", "2015", "2016", "2017"],
+        ["talha", 56.5, 82.1, 88.7, 70.1, 53.4, 85.1],
+        ["sj", 51.1, 51.4, 55.1, 53.3, 73.8, 68.7],
+        ["rachan", 40.1, 62.2, 69.5, 36.4, 45.2, 32.5],
+        ["vivek", 25.2, 37.1, 41.2, 18, 33.9, 49.1]
     ]
     return jsonify(dataset)
 
